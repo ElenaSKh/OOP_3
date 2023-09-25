da38ceca380546de0f6f367201040be97af3947d
Elena Haritonova <helens.kharitonova@gmail.com>
2023-09-25 16:57:12 +0300

    Add other files

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# Задача 1
+print('Задача 1, супергерои')
+
 import json
 from pprint import pprint
 import requests
@@ -15,3 +18,41 @@ super_hero = super_heros[-1]
 pprint(super_heros)
 print('**************')
 print(f"Самый умный {super_hero['name']}, интеллект: {super_hero['intelligence']}")
+
+
+# Задача 2
+print('Задача 2, Яндекс.Диск')
+
+import requests
+import os
+
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def upload(self, file_path: str):
+        # Тут ваша логика
+        url = 'https://cloud-api.yandex.net/v1/disk/resources'
+        params = {"path": file_path}
+        headers = {"Authorization": token}
+        response = requests.put(url, headers=headers, params=params)
+        url = url + '/upload'
+        print(url)
+        file_in_dir = os.listdir(file_path)
+        for file_i in file_in_dir:
+            file_full_path = file_path + '/' + file_i
+            params = {"path": file_full_path}
+            resp = requests.get(url, headers=headers, params=params)
+            url_for_upload = resp.json().get('href', '')
+            print(file_full_path)
+            with open(file_full_path, 'rb') as file:
+                response2 = requests.put(url_for_upload, files={"file": file})
+            print(f'файл {url_for_upload} \n загружен!')
+        # Функция может ничего не возвращать
+
+if __name__ == '__main__':
+    # Получить путь к загружаемому файлу и токен от пользователя
+   token = ''
+    path_to_file = 'C:\\Education\\PycharmProjects\\OOP3\\superheroes.jpg'
+    uploader = YaUploader(token)
+    result = uploader.upload(path_to_file)
