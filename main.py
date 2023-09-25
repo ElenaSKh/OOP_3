680a2d68032cdfa4ef26501bd310f5560c0bfb28
Elena Haritonova <helens.kharitonova@gmail.com>
2023-09-25 19:13:13 +0300

    Corrected main file

--- main.py
+++ main.py
@@ -31,28 +31,21 @@ class YaUploader:
         self.token = token
 
     def upload(self, file_path: str):
+        """Метод загружает файлы по списку file_list на яндекс диск"""
         # Тут ваша логика
-        url = 'https://cloud-api.yandex.net/v1/disk/resources'
-        params = {"path": file_path}
-        headers = {"Authorization": token}
-        response = requests.put(url, headers=headers, params=params)
-        url = url + '/upload'
-        print(url)
-        file_in_dir = os.listdir(file_path)
-        for file_i in file_in_dir:
-            file_full_path = file_path + '/' + file_i
-            params = {"path": file_full_path}
-            resp = requests.get(url, headers=headers, params=params)
-            url_for_upload = resp.json().get('href', '')
-            print(file_full_path)
-            with open(file_full_path, 'rb') as file:
-                response2 = requests.put(url_for_upload, files={"file": file})
-            print(f'файл {url_for_upload} \n загружен!')
         # Функция может ничего не возвращать
+        url = 'https://cloud-api.yandex.net:443/v1/disk/resources/upload'
+        headers = {'Authorization': 'OAuth ' +self.token}
+        params = {'path': '/superheroes.jpg'}
+        data = requests.get(url, headers=headers, params=params).json()
+        url = data['href']
+        with open(file_path, 'rb') as f:
+            response = requests.put(url, files={'file': f}, headers=headers, params=params)
+        return response.status_code
 
 if __name__ == '__main__':
     # Получить путь к загружаемому файлу и токен от пользователя
-   token = ''
-    path_to_file = 'C:\\Education\\PycharmProjects\\OOP3\\superheroes.jpg'
+    path_to_file = "C:\\Education\\PycharmProjects\\OOP3\\superheroes-0.jpg"
+    token = ''
     uploader = YaUploader(token)
     result = uploader.upload(path_to_file)
